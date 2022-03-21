6ce7d08cf0cc00e71c284b3467971d2262cf4a52
Raquel D <raqueldantasbatista@gmail.com>
2022-03-21 09:56:44 +0100

    Organizing input data

--- src/DFMesh.py
+++ src/DFMesh.py
@@ -2,22 +2,32 @@ import numpy as np
 import itertools
 import copy 
 
-# Mesh
+# Lenght of the bar (L)
 L = 50*10**-3  # (m)
+# Number of linear elements (n_el)
 n_el = 3
+# Lenght of each linear element (h)
 h = L/n_el
 
 # Material id convention: 
-# 0(line elemnt)/1(interface element)/2(BC left node)/3(BC right node)/ 4(Velocity right node)/5(Velocity right node)
+# 0 : line elemnt
+# 1 : interface element
+# 2 : Support left node
+# 3 : Support right node 
+# 4 : Velocity applied left node
+# 5 : Velocity applied right node
 materials = [0] * n_el
-materials.append(2)
+# materials.append(2) # Support
 materials.append(4)
+materials.append(5)
+
 # node_id[elem_index][local_node], returns the global node id
 node_id = []
 for i in range(n_el):
     node_id.append([i, i+1])
-# Node BC
-node_id.append([0]) # Support
+# Identify each node has apllied BCs
+# node_id.append([0]) # Support
+node_id.append([0]) # Applied velocity at left boundary
 node_id.append([n_el]) # Applied velocity at right boundary 
 
 # Connect[el][j] returns the global index of local dof 'j' of the element 'el'
@@ -25,55 +35,56 @@ connect = copy.deepcopy(node_id)
 
 # Applied strain rate and veloctities
 strain_rate = 10.0**1  # (s-1)
-vel = strain_rate*L
+vel = strain_rate*L/2
 
-# BC
+# BC dictionary
 bc_dict = {
     2: (0, "dirichlet"),
-    4: (vel, "velocity"),
-    5: (-vel, "velocity")
+    4: (-vel, "velocity"),
+    5: (vel, "velocity")
 }
+# Number of degree of freedom 
 n_dofs = max(list(itertools.chain.from_iterable(connect))) + 1
 
-
-
-# Input parameters
-
-# Material parameters
+# Young's module 
 E = 275.0*10**9  # (Pa)
-
+# Cross sectional area
 A = 1*10**-3  # (unit area)
+# Fracture energy
 Gc = 100.0  # (N/m)
+# Limit stress
 stress_c = 300.0*10**5  # (Pa)
-rho = 75.0*10**3  # (kg/m3)
+# Limit fracture oppening
 delta_c = (2.0*Gc)/stress_c
-
-# Time steps
-n_steps = 85
-
-
-
-# Initial displacement
-u0 = np.zeros((n_dofs))
-
-# Initial velocity (v0): velocity profile (vel) is a function v(x)
-n_points = n_dofs
-l = np.linspace(0, L, n_points)
-v0 = np.array([vel/L*x for x in l])
-v0 = np.round(v0, 8)
-
-
-# Initial acceleration (acel0)
-acel0 = np.zeros((n_dofs))
-# Load (p)
-p = np.zeros((n_steps+1, n_dofs))
-
-# Initialization of maximum jump u between two linear elements
-delta_max = np.zeros((len(materials)*2))
+# Density
+rho = 75.0*10**3  # (kg/m3)
 
 # Time integration
+
+# Number of time steps (n_steps)
+n_steps = 85
+# Critical time step
 dt_crit = h/((E/rho)**0.5)
+# Adopted time step
 dt = dt_crit*0.1  # (s)
+# Newmark explicity constants
 gamma = 0.5
 beta = 0.0
+
+# Initial values
+
+# Initial displacement (u0)
+u0 = np.zeros((n_dofs))
+# Initial velocity (v0): velocity profile (vel) is a function v(x)
+n_points = n_dofs
+l = np.linspace(-L/2, L/2, n_points)
+v0 = np.array([strain_rate*x for x in l])
+v0 = np.round(v0, 8)
+# Initial acceleration (acel0)
+acel0 = np.zeros((n_dofs))
+# Inital load (p)
+p = np.zeros((n_steps+1, n_dofs))
+# Damping
 C = np.zeros((n_dofs, n_dofs))
+# Initialization of maximum jump u between two linear elements (delta_max)
+delta_max = np.zeros((len(materials)*2))
