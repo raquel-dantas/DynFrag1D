63364fddb3a87f1ce6f9cde58a94635b3039d06f
Raquel D <raqueldantasbatista@gmail.com>
2022-06-26 14:04:18 +0200

    draft comparison analytical form

--- src/DFAnalyticalForm.py
+++ src/DFAnalyticalForm.py
@@ -16,6 +16,16 @@ def GradyFragSize(values_strainrate):
     return s
 
 
+
+def GradyFragSizeS(epsilon):
+    """Returns the fragment size estimation through analytical formulation proposed by Grady (1982) for a given interval of strain rate."""
+
+    s = (24.* DFMesh.Gc)/(DFMesh.rho * epsilon**2)**(1/3)
+
+    return s
+
+
+
 def GlenChudnoviskFragSize(values_strainrate):
 
     for epsilon in range(len(values_strainrate)):
@@ -33,25 +43,30 @@ min_strainrate = 10**1
 max_strainrate= 10**10
 values_strainrate = np.linspace(min_strainrate, max_strainrate, num=points_interval)
 s_grady = GradyFragSize(values_strainrate)
-s_gc = GlenChudnoviskFragSize(values_strainrate)
+# s_gc = GlenChudnoviskFragSize(values_strainrate)
 
 
-def PlotMultLog():
-
-    fig, axes = plt.subplots()
-    axes.grid(True, which='both')
-    axes.axhline(y=0, color='k')
-    plt.title("Stress evolution")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Stress (Pa)")
-
-    x = np.linspace(0, DFMesh.time_simulation, DFMesh.n_steps)
-    for el in range(len(DFMesh.materials)):
-        plt.plot(x, stress_evl[el], label=el)
-    plt.legend()
-    plt.show()
+s_gr = GradyFragSizeS(10**4)
+print(s_gr)
 
 
+# def PlotMultLog():
+
+#     fig, axes = plt.subplots()
+#     axes.grid(True, which='both')
+#     axes.axhline(y=0, color='k')
+#     plt.title("Stress evolution")
+#     plt.xlabel("Time (s)")
+#     plt.ylabel("Stress (Pa)")
+
+#     x = np.linspace(0, DFMesh.time_simulation, DFMesh.n_steps)
+#     for el in range(len(DFMesh.materials)):
+#         plt.plot(x, stress_evl[el], label=el)
+#     plt.legend()
+#     plt.show()
 
 
-DFPlot.Plotlog(values_strainrate, s, "strain rate (s-1)", "Frag. size (m)", "Grady (1982)")
+
+
+# DFPlot.Plotlog(values_strainrate, s_grady, "strain rate (s-1)", "Frag. size (m)", "Grady (1982)")
+# DFPlot.Plotlog(values_strainrate, s_gc, "strain rate (s-1)", "Frag. size (m)", "Grady (1982)")
