71ff2a9ddd8bdc653ec5655b39eeb858e60a4073
Raquel D <raqueldantasbatista@gmail.com>
2022-07-06 10:30:22 +0200

    Draft analytical models for estimation fragsize

--- src/DFFragmentation.py
+++ src/DFFragmentation.py
@@ -1,7 +1,10 @@
 import numpy as np
 import DFMesh
 import DFFem
+import DFPlot
 
+
+# Number and size of fragments
 
 def NumberFragments(damage):
     return 1 + sum(1 for D in damage if D > 0.999)
@@ -25,4 +28,36 @@ def SizeFragments(damage):
     
     return fraglen, np.average(fraglen)
 
+
+# Analytical formulation for average fragment size
+
+def GradyFragSize(values_strainrate):
+    """Returns the fragment size estimation through analytical formulation proposed by Grady (1982) for a given interval of strain rate."""
+
+    s = np.array([((24.* DFMesh.Gc)/(DFMesh.rho * epsilon**2))**(1/3) for epsilon in values_strainrate])
+
+    return s
+
+
+
+def GlenChudnoviskFragSize(values_strainrate):
+    """Returns the fragment size estimation through analytical formulation proposed by Glen and Chudnovisk (1986) for a given interval of strain rate."""
+
+    s = np.zeros(len(values_strainrate))
+    for i in range(len(values_strainrate)):
+        alpha = 3.*DFMesh.stress_c / (DFMesh.rho * DFMesh.E * values_strainrate[i]**2)
+        beta = 3.*DFMesh.Gc / (2.* DFMesh.rho * DFMesh.E * values_strainrate[i]**2)
+        phi = np.arcsinh(beta*(3./alpha)**(3/2))
+        s[i] = 4.*(alpha/3.)*np.sinh(phi/3.)
+
+    return s
+
+
+
+def ZhouMolinariRameshFragSize(values_strainrate):
+    """Returns the fragment size estimation through analytical formulation proposed by Zhou, Molinari and Ramesh (2006) for a given interval of strain rate."""
+
+    s = np.array([4.5 / (1 + 4.5*epsilon**(-2/3)) for epsilon in values_strainrate])
+
+    return s
 
