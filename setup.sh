cab290546ac747b6424e20e7f83f82fe1ef59fbf
Raquel D <raqueldantasbatista@gmail.com>
2022-07-13 11:49:56 +0200

    update setup to source akantu python interface

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/usr/local/bin/sh
 
-python3 -m venv .venv && \
-source .venv/bin/activate && \
-pip3 install ipykernel numpy matplotlib progressbar akantu autopep8
+set -e # exit script on error
+
+# Create and source virtual environment
+python3 -m venv .venv
+source .venv/bin/activate 
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Source akantu built from source 
+source ~/projects/lib/akantu/build/akantu_environement.sh
+
+
+set +e # return to default shell behaviour (You probably want to keep this at the end of this script)
