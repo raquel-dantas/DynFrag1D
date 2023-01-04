d533e7f0efe9c38bf4c3eff5d79c209568d061b2
Raquel D <raqueldantasbatista@gmail.com>
2023-01-04 16:15:29 +0100

    Add getFunctional() functions

--- src_lipfield/DFDamage.py
+++ src_lipfield/DFDamage.py
@@ -1,4 +1,10 @@
 import DFMesh
+import numpy as np
+import scipy
+from scipy.optimize import minimize
+from scipy.optimize import LinearConstraint
+from itertools import groupby
+from operator import itemgetter
 
 
 # Regularization inputs
@@ -20,3 +26,40 @@ def h_lip(lamb,d): return (2.*d - d**2) / (1. - d + lamb * d**2)**2
 def h_czm(lamb,d): return 1./(1.-lamb)*((1./((1.-lamb)*(1.-d)**2 + lamb))-1.)
 # h = h_czm
 h = h_lip
+
+
+
+# Functional given by Moes, Le and Stershic (2022)
+def getFunctional(strain): 
+    """Returns a lambda function of damage to be use in the optimization problem for the whole domain.\n
+    Arguments: \n
+    strain -- strain computed using the displacement at the next time-step(n+1)."""
+    
+    functional_whole_domain = lambda damage: w*sum([
+        (0.5*(1. - damage[el])**2 * 
+        DFMesh.E*strain[el]**2 + 
+        Yc[el] * 
+        h(lamb[el], damage[el])) * 
+        DFMesh.hun*0.5
+        for el in range(DFMesh.n_el)
+        ])
+    
+    return functional_whole_domain
+
+
+def getFunctionalSubdomain(strain, region_optimization): 
+    """Returns a lambda function of damage to be use in the optimization problem for a specific region in the domain (subdomain).\n
+    Arguments: \n
+    strain -- strain computed using the displacement at the next time-step(n+1).\n
+    region -- an array with the element indexes of the subdomain to compute damage at next time-step(n+1)."""
+
+    functional_subdomain = lambda damage: w*sum([
+        (0.5*(1. - damage[region_optimization[i]])**2 * 
+        DFMesh.E*strain[region_optimization[i]]**2 + 
+        Yc[region_optimization[i]] * 
+        h(lamb[region_optimization[i]], damage[i])) * 
+        DFMesh.hun*0.5
+        for i in range(len(region_optimization))
+        ])
+    
+    return functional_subdomain
