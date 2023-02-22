9574e83d65141572d1fdbba6c8bd6932469d82f8
Raquel D <raqueldantasbatista@gmail.com>
2023-02-22 17:29:55 +0100

    refactor of input data

---
+++ src/input_files/input_data.py
@@ -0,0 +1,36 @@
+# Input model 1D dynamic fragmentation of an expanding ring
+
+
+# Method
+use_1d_cohesive_elements = True
+use_akantu = False
+use_lipfield = False
+
+
+# Type of mesh
+uniform_mesh = True
+create_mesh = True
+# if there is an mesh file for input set create_mesh = False
+
+
+# Material
+young_modulus = 275.0 * 10**9  # (Pa)
+density = 2750.0  # (kg/m3)
+fracture_energy = 100.0  # (N/m)
+stress_limit = 300.0 * 10**6  # Limit stress (Pa)
+
+
+# Geometry
+bar_length = 5 * 10**-3  # (m)
+x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
+xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
+number_elements = 250  
+area = bar_length / number_elements  # Cross sectional area (m2) (Equal to element size )
+
+
+# Load
+strain_rate = 10.0**4  # (s-1)
+
+
+# Time
+time_simulation = 3.0 * 10**-7  # Total time of simulation (s)
