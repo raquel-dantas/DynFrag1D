19b5e8533323810cc723e67f6d26e67c619d788a
Raquel D <raqueldantasbatista@gmail.com>
2023-02-27 17:52:27 +0100

    add create random stress_limit  in separate file

---
+++ src/input_files/generate_random_limit_stress.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pickle
+
+# This file generate a random variation around stress limit value pickle file to be read in DFMesh.py if generate_limit_stress_variation == False
+
+n_elements = 250
+
+stress_limit = 300.0 * 10**6  # (Pa)
+
+stress_critical = np.random.uniform(
+    low=stress_limit - 1.0 * 10**6,
+    high=stress_limit + 1.0 * 10**6,
+    size=(n_elements),
+)
+
+with open('src/input_files/random_stress_critical.pickle', 'wb') as handle:
+    pickle.dump(stress_critical, handle, protocol=pickle.HIGHEST_PROTOCOL)
