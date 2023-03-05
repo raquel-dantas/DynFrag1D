c63624e8c3dba391f6431af0372bad533c7b808c
Raquel D <raqueldantasbatista@gmail.com>
2023-03-05 14:44:50 +0100

    input test validate lipfield

--- src/input_files/input_data.py
+++ src/input_files/input_data.py
@@ -7,29 +7,29 @@ use_lipfield = True
 
 
 # Type of mesh
-uniform_mesh = False
+uniform_mesh = True
 create_mesh = True # if there is an mesh file for input set create_mesh = False
 
 
 # Material
-young_modulus = 380 * 10**9  # (Pa)
-density = 3.9 * 10 **3  # (kg/m3)
-fracture_energy = 83.13  # (N/m)
-stress_limit = 100.0 * 10**6  # (Pa)
+young_modulus = 275.0*10**9   # (Pa)
+density = 2750.0 # (kg/m3)
+fracture_energy = 100.  # (N/m)
+stress_limit = 300.0 * 10**6  # (Pa)
 generate_limit_stress_variation = True # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
 
 
 # Geometry
-bar_length = 2 * 10**-3  # (m)
-x0 = 0  # Left extremitiy x coordinate / 0-initial
-xf = bar_length  # Rigth extremitiy x coordinate / f-final
-number_elements = 1500  
-area = 2e-7  # Cross sectional area (m2) (Equal to element size )
+bar_length = 5 * 10** -3  # (m)
+x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
+xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
+number_elements = 500  
+area = 1. * 10 **-3  # Cross sectional area (m2) (Equal to element size )
 
 
 # Load
-strain_rate = 10.0**5  # (s-1)
+strain_rate = 10.0**4  # (s-1)
 
 
 # Time
-time_simulation = 1.0 * 10**-7  # Total time of simulation (s)
+time_simulation = 3.0 * 10**-7  # Total time of simulation (s)
