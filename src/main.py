8b160b598434f1260d65d3de7e491da92b9f059a
Raquel D <raqueldantasbatista@gmail.com>
2023-03-07 14:41:23 +0100

    add save at all steps and change plots

--- src/main.py
+++ src/main.py
@@ -18,24 +18,30 @@ def runSimulation(strain_rate):
     # Initiation of variables
 
     u = DFModel.u
+    u_all_steps = DFModel.u_all_steps
     v = DFModel.v
     acel = DFModel.acel
     d = DFModel.d
+    damage_all_steps = DFModel.damage_all_steps
     avg_stress_bar = DFModel.avg_stress_bar
-
+    stress_all_steps = DFModel.stress_all_steps
     data_bc = DFModel.data_bc
     work_previous_step = DFModel.work_previous_step
     energies = DFModel.energies
 
     n_fragments = DFModel.n_fragments
     avg_frag_size = DFModel.avg_frag_size
+    fraglen_all_steps = DFModel.fraglen_all_steps
     data_histogram_frag_size = []
+
+
 
     for n in range(DFModel.n_init, DFModel.n_final):
 
         DFModel.updateProgressBar(n, bar)
 
         stress, average_stress_neighbors = DFPostProcess.computeStress(u, d)
+        stress_all_steps.append(stress)
         avg_stress_bar[n] = DFPostProcess.stressBar(stress)
         M, F = DFFem.globalSystem()
 
@@ -43,6 +49,8 @@ def runSimulation(strain_rate):
 
         if DFMesh.use_cohesive_elements == True:
             d = DFInterface.getDamageParameter()
+        
+        damage_all_steps.append(d)
 
         energies = DFPostProcess.updateEnergies(
             energies, n, u, v, d, stress, data_bc, work_previous_step
@@ -55,47 +63,51 @@ def runSimulation(strain_rate):
         data_histogram_frag_size = DFFragmentation.getFragSizeHistogramData(
             frag_lengths
         )
+        fraglen_all_steps.append(frag_lengths)
 
         # Save results previous step at BC to compute external work
         data_bc = DFPostProcess.saveResultsAtBC(u, d)
 
         # Time integration
         u, v, acel, d = DFNewmark.explicitScheme(M, u, v, acel, d, F, DFMesh.dt)
+        u_all_steps.append([u])
 
         if DFMesh.use_cohesive_elements == True:
             u, v, acel = DFInterface.verifyStress(average_stress_neighbors, u, v, acel)
 
     DFModel.endProgressBar(bar)
 
-    DFPlot.plotAverageStressBar(avg_stress_bar)
-    DFPlot.plotNumberFragments(n_fragments)
-    DFPlot.plotFragmentSizeHistogram(frag_lengths, 10)
+
 
     # Energy balance
     var_energies = DFPostProcess.computeVariationEnergy(energies)
     power = DFPostProcess.computePower(energies)
-
+    # Plots
+    DFPlot.plotDamage(d)
+    DFPlot.plotAverageStressBar(avg_stress_bar)
+    DFPlot.plotNumberFragments(n_fragments)
+    DFPlot.plotFragmentSizeHistogram(frag_lengths, 10)
     DFPlot.plotEnergies(energies)
     DFPlot.plotVarEnergies(var_energies)
     DFPlot.plotPower(power)
-
-    if DFMesh.use_lipfield == True:
-        DFPlot.plotByIntPoint(d)
-
     # Save results
     DFPlot.saveResults(u)
     DFPlot.saveResults(v)
     DFPlot.saveResults(acel)
     DFPlot.saveResults(d)
-
+    DFPlot.saveResults(u_all_steps)
+    DFPlot.saveResults(damage_all_steps)
+    DFPlot.saveResults(stress_all_steps)
     DFPlot.saveResults(n_fragments)
     DFPlot.saveResults(avg_frag_size)
     DFPlot.saveResults(data_histogram_frag_size)
-
     DFPlot.saveResults(avg_stress_bar)
     DFPlot.saveResults(energies)
     DFPlot.saveResults(var_energies)
     DFPlot.saveResults(power)
+    if DFMesh.use_cohesive_elements == True:
+        DFPlot.saveResults(DFMesh.materials)
+        DFPlot.saveResults(DFMesh.connect)
 
 
 if __name__ == "__main__":
@@ -103,7 +115,4 @@ if __name__ == "__main__":
     runSimulation(DFMesh.strain_rate)
     total_time = time.time() - start_time
     print("--- %s seconds ---" % (time.time() - start_time))
-    if DFMesh.use_cohesive_elements == True:
-        DFPlot.saveResultsCZM(total_time)
-    if DFMesh.use_lipfield == True:
-        DFPlot.saveResultsLipfield(total_time)
+    DFPlot.saveResults(total_time)
