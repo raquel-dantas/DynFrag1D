04dfc38d432165d90a8150490b70533c1fe6a4ba
Raquel D <raqueldantasbatista@gmail.com>
2023-03-07 16:16:30 +0100

    add use of symmetry variable

--- src/input_files/input_data.py
+++ src/input_files/input_data.py
@@ -2,15 +2,15 @@
 
 
 # Method
-use_cohesive_elements = True
-use_lipfield = False
+use_cohesive_elements = False
+use_lipfield = True
 
 
 # Type of mesh
 uniform_mesh = True
 create_mesh = True # if there is an mesh file for input set create_mesh = False
 if create_mesh == False:
-    mesh_file_name = 'src/input_files/mesh_non_uniform.pickle'
+    mesh_file_name = 'src/input_files/mesh_uniform_600.pickle'
 
 
 # Material
@@ -19,17 +19,17 @@ density = 2750.0 # (kg/m3)
 fracture_energy = 100.  # (N/m)
 stress_limit = 300.0 * 10**6  # (Pa)
 
-generate_limit_stress_variation = True # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
+generate_limit_stress_variation = False # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
 if generate_limit_stress_variation == False:
-    stress_limite_file_name = 'src/input_files/random_stress_critical.pickle'
+    stress_limite_file_name = 'src/input_files/random_stress_critical_600.pickle'
 
 
 # Geometry
-bar_length = 5 * 10** -3  # (m)
+bar_length = 5. * 10** -3  # (m)
 x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
 xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
-number_elements = 10 
-area = 1. * 10 **-3  # Cross sectional area (m2) (Equal to element size )
+number_elements = 600
+area = 1*10**-5  # Cross sectional area (m2) (Equal to element size )
 
 
 # Load
@@ -44,3 +44,6 @@ initial_step = 0
 continue_simulation_from_step = False
 # if there is previous data to continue the simulation from a previous simulation set 
 # continue_simulation_from_step = True and give the time to start the simulation
+
+half_bar = False
+# if use symmetry we have to add the bc proper
