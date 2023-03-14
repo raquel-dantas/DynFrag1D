0dc456ddcd00d29bddc103ed4970c3a85781932e
Raquel D <raqueldantasbatista@gmail.com>
2023-03-14 11:24:34 +0100

    import input 7500el

--- src/DFMesh.py
+++ src/DFMesh.py
@@ -4,7 +4,7 @@ import copy
 import pickle
 
 # import input_files.input_data as inputdata
-import input_files.input_2.input2_data_non_un_mesh_2500 as inputdata
+import input_files.input_2.input2_data_non_un_mesh_7500 as inputdata
 filepath = inputdata.filepath
 
 # Import or set inputs
