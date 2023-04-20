4636bdcecc926adacadca348fb883f17033dd6d7
Raquel D <raqueldantasbatista@gmail.com>
2023-04-20 15:44:46 +0200

    input format for use akantu

--- src_akantu/input_files/input_papers_czm/input_data_uniform_mesh.py
+++ src_akantu/input_files/input_papers_czm/input_data_uniform_mesh.py
@@ -2,46 +2,47 @@
 
 # Type of mesh
 uniform_mesh = True
-# if there is an mesh file for input set create_mesh = False
-create_mesh = True 
+create_mesh = True  # if there is an mesh file for input set create_mesh = False
 if create_mesh == False:
-    mesh_file_name = 'filename.pickle'
+    mesh_file_name = "filename.pickle"
+
 
 # Material
-young_modulus = 275.0*10**9   # (Pa)
-density = 2750.0 # (kg/m3)
-fracture_energy = 100.  # (N/m)
+young_modulus = 275.0 * 10**9  # (Pa)
+density = 2750.0  # (kg/m3)
+fracture_energy = 100.0  # (N/m)
 stress_limit = 300.0 * 10**6  # (Pa)
 
-# if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
-generate_limit_stress_variation = False 
+generate_limit_stress_variation = False  # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
 if generate_limit_stress_variation == False:
-    stress_limit_file_name = 'src_akantu/input_files/random_stress_files/random_stress_critical_1250.pickle'
+    stress_limite_file_name = (
+        "src/input_files/random_stress_files/random_stress_critical_1250.pickle"
+    )
 
 
 # Geometry
-bar_length = 50 * 10** -3  # (m)
+bar_length = 50.0 * 10**-3  # (m)
 x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
 xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
-number_elements = 1250 * 2 # Total number of triangular elements
-area = 1.  # Cross sectional area (m2) (Equal to element size )
+number_elements = 1250 * 2
+area = 1.0  # Cross sectional area (m2) (Equal to element size )
 
 
 # Load
-strain_rate = 10.0**4  # (s-1)
+strain_rate = 10.0**5  # (s-1)
 
 
 # Time
-time_simulation = 6.5 * 10**-7   # Total time of simulation (s)
+time_simulation = 2.0 * 10**-7  # Total time of simulation (s)
 
-# if there is previous data to continue the simulation from a previous simulation set 
-# continue_simulation_from_step = True and give the time to start the simulation
+
+# if there is previous data to continue the simulation set continue_simulation_from_step = True and give the time to start the simulation and the files path
 initial_step = 0
 continue_simulation_from_step = False
-
+if continue_simulation_from_step == True:
+    previous_simulation = "filename.pickle"
 
 half_bar = False
-# if use symmetry we have to add the bc proper
+# if use symmetry we have to add the bc properly
 
-filepath = "src_akantu/LOG/mesh_study/czm_uniform_mesh/1250el/"
-
+filepath_save_results = "output/uniform_mesh_1250el/"
