1e213f12677a7688067b2881ae65a875970df07c
Raquel D <raqueldantasbatista@gmail.com>
2023-04-20 09:34:40 +0200

    set the input file as input_data

--- src/input_files/input_data.py
+++ src/input_files/input_data.py
@@ -9,29 +9,29 @@ use_lipfield = True
 # Type of mesh
 uniform_mesh = True
 # if there is an mesh file for input set create_mesh = False
-create_mesh = True 
+create_mesh = True
 if create_mesh == False:
-    mesh_file_name = 'filename.pickle'
+    mesh_file_name = "filename.pickle"
 
 
 # Material
-young_modulus = 275.0*10**9   # (Pa)
-density = 2750.0 # (kg/m3)
-fracture_energy = 300.  # (N/m)
+young_modulus = 275.0 * 10**9  # (Pa)
+density = 2750.0  # (kg/m3)
+fracture_energy = 300.0  # (N/m)
 stress_limit = 300.0 * 10**6  # (Pa)
 
 # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
-generate_limit_stress_variation = True 
+generate_limit_stress_variation = True
 if generate_limit_stress_variation == False:
-    stress_limite_file_name = 'filename.pickle'
+    stress_limite_file_name = "filename.pickle"
 
 
 # Geometry
-bar_length = 50 * 10** -3  # (m)
+bar_length = 50 * 10**-3  # (m)
 x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
 xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
 number_elements = 1250
-area = 1.  # Cross sectional area 
+area = 1.0  # Cross sectional area
 
 
 # Load
@@ -43,13 +43,13 @@ time_simulation = 3.5 * 10**-7  # Total time of simulation (s)
 
 
 # if there is previous data to continue the simulation set continue_simulation_from_step = True and give the time to start the simulation and the files path
-initial_step = 498
-continue_simulation_from_step = True
+initial_step = 0
+continue_simulation_from_step = False
 if continue_simulation_from_step == True:
-    previous_simulation = "LOG/lipfield_test/lipfield_step_498_.pickle"
+    previous_simulation = "filename.pickle"
 
 
 half_bar = False
 # if use symmetry we have to add the bc proper
 
-filepath_save_results = "LOG/lipfield_test/"
+filepath_save_results = "output/"
