e488bc64262b9c0725b5d2007a55c84eedb20a71
Raquel D <raqueldantasbatista@gmail.com>
2023-05-01 11:08:24 +0200

    use input for uniform mesh

--- input_files/input_data_aka.py
+++ input_files/input_data_aka.py
@@ -2,9 +2,9 @@
 import subprocess
 
 # Type of mesh
-uniform_mesh = False
+uniform_mesh = True
 # if there is an mesh file for input set create_mesh = False
-create_mesh = False
+create_mesh = True
 if create_mesh == False:
     mesh_file_name = "input_files/mesh_non_uniform.pickle"
 
@@ -15,7 +15,7 @@ fracture_energy = 100.0  # (N/m)
 stress_limit = 300.0 * 10**6  # (Pa)
 
 # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
-generate_limit_stress_variation = False
+generate_limit_stress_variation = True
 if generate_limit_stress_variation == False:
     stress_limit_file_name = "input_files/random_stress_critical.pickle"
 
@@ -24,7 +24,7 @@ if generate_limit_stress_variation == False:
 bar_length = 50 * 10**-3  # (m)
 x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
 xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
-number_elements = 1250 * 2  # Total number of triangular elements
+number_elements = 5000 * 2 # Total number of triangular elements
 area = 1.0  # Cross sectional area (m2) (Equal to element size )
 
 
