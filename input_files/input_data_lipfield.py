8dc1a9b32836f750ae304106ab64299459a4d461
Raquel D <raqueldantasbatista@gmail.com>
2023-05-08 16:01:37 +0200

    set base input file

--- input_files/input_data_lipfield.py
+++ input_files/input_data_lipfield.py
@@ -11,46 +11,45 @@ uniform_mesh = False
 # if there is an mesh file for input set create_mesh = False
 create_mesh = False
 if create_mesh == False:
-    mesh_file_name = "input_files/mesh_non_uniform.pickle"
+    mesh_file_name = "input_files/mesh_files/filename.pickle"
 
 
 # Material
-young_modulus = 275.0 * 10**9  # (Pa)
-density = 2750.0  # (kg/m3)
+young_modulus = 275e9  # (Pa)
+density = 2.75e3  # (kg/m3)
 fracture_energy = 100.0  # (N/m)
-stress_limit = 300.0 * 10**6  # (Pa)
+stress_limit = 300e6  # (Pa)
 
 # if there is already a file with the random values for limit stress set generate_limit_stress_variation = False
 generate_limit_stress_variation = False
 if generate_limit_stress_variation == False:
-    stress_limite_file_name = "input_files/random_stress_critical.pickle"
+    stress_limite_file_name = "input_files/random_stress_files/filename.pickle"
 
 
 # Geometry
-bar_length = 50 * 10**-3  # (m)
+bar_length = 50e-3  # (m)
 x0 = -0.5 * bar_length  # Left extremitiy x coordinate / 0-initial
 xf = 0.5 * bar_length  # Rigth extremitiy x coordinate / f-final
-number_elements = 1250
+number_elements = 625
 area = 1.0  # Cross sectional area
 
 
 # Load
-strain_rate = 10.0**4  # (s-1)
+strain_rate = 10.0**5  # (s-1)
 
 
 # Time
-time_simulation = 4.0 * 10**-7  # Total time of simulation (s)
+time_simulation = 2.0 * 10**-7  # Total time of simulation (s)
 
 
 # if there is previous data to continue the simulation set continue_simulation_from_step = True and give the time to start the simulation and the files path
 initial_step = 0
 continue_simulation_from_step = False
 if continue_simulation_from_step == True:
-    previous_simulation = "filename.pickle"
+    previous_simulation = "output/10to5/lipfield_uniform/filename.pickle"
 
 
-half_bar = False
 # if use symmetry we have to add the bc proper
+half_bar = False
 
-subprocess.Popen("mkdir output", shell=True)
-filepath_save_results = "output/"
+filepath_save_results = "output/10to5/lipfield_uniform_625/"
